242a6a6a0ffef2ce767fdb8f4f21b3b9ea66416f
Jurriaan Bremer <jurriaanbremer@gmail.com>
2013-06-22 21:03:35 +0200

    fix losing memory in ctypes, add easy register access in py bindings

--- darmu.py
+++ darmu.py
@@ -8,7 +8,7 @@ class _Darmu(Structure):
     ]
 
 
-class Darmu:
+class Darmu(object):
     def __init__(self, stack_size):
         self.stack = create_string_buffer(stack_size)
 
@@ -16,19 +16,34 @@ class Darmu:
 
         _lib.darmu_init(self.darmu, self.stack, stack_size)
 
+        self._gc = [self.stack]
+
     def set_argv_env(self, argv, env):
         # TODO initialize the contents of the stack
         pass
 
     def entry_point(self, address):
-        _lib.darmu_register_set(self.darmu, 0b1111, address)
+        self.r15 = address
 
     def mapping(self, image, raw_size, address):
+        self._gc.append(image)
         _lib.darmu_mapping_add(self.darmu, image, raw_size, address)
 
     def single_step(self):
         ret = _lib.darmu_single_step(self.darmu)
         return ret == 0
+
+    def _reg(idx):
+        def _read(self):
+            return _lib.darmu_register_get(self.darmu, idx)
+
+        def _write(self, value):
+            _lib.darmu_register_set(self.darmu, idx, value)
+
+        return property(_read, _write)
+
+    for _ in xrange(16):
+        locals()['r%d' % _] = _reg(_)
 
 
 def _set_func(name, restype, *argtypes):
